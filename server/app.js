7dc0f9ee9df03dc8d4a4045c0f1e5005ac8484f1
Yaman Ziadeh <yamanziadeh@Yamans-MBP.hsd1.il.comcast.net>
2018-11-04 11:17:14 -0600

    test

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-      app = express
+      app = express(),
       server = app.listen(3000, function(){
           console.log("Listening on Port 3000...");
       });
