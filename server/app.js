7e0ddb07fef22b40bb08a3afd49c7767e20ab0df
trujic1000 <aleksandar.trujic@live.com>
2018-11-06 09:16:42 -0600

    minor changes in folder structure

--- server/app.js
+++ server/app.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // CONFIG FILE FOR DEV, TEST OR PROD ENVIRONMENT
-const config = require('./config/config');
-// CONNECTING TO THE DB
-const { mongoose } = require('./db/mongoose');
+const config = require('./config');
 
 const app = express();
 
 // MIDDLEWARES
 require('./middleware/middleware')(app);
 
-
 app.listen(config.PORT, () => {
+  mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
+});
+
+const db = mongoose.connection;
+
+db.once('open', () => {
   console.log(`Listening on port ${config.PORT}`);
 });
